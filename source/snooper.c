912a3224707803de4ddd0188f8f55eb0451d8bc1
Narongsak Mala <pepsi_kmutnb@windowslive.com>
2014-09-28 21:55:58 +0700

    Update snoop.c
    Update Reversion history and reformat.

--- source/snooper.c
+++ source/snooper.c
@@ -1,3 +1,13 @@
+/* 
+	Copyright (C) 2014, by Narongsak mala
+	Created By    : Narongsak Mala
+	created Date  : 28/09/2014
+	Modified by   : Narongsak mala
+	Modified date : 28/09/2014
+	
+	Reversion history
+	1.0.0 Intialize.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,17 +15,15 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
-//#include <pcap.h>
 
 #define STREQ(a, b) (strcmp(a, b) == 0)
 
-static char HEXTABLE[] = {'0','1',
-	'2','3','4','5','6','7','8','9',
-	'a','b','c','d','e','f'};
-
+static char HEXTABLE[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 static int src_port = 0;
 static int dst_port = 0;
-char * dec_to_hex(char c){
+
+char* 
+dec_to_hex(char c){
 	static char b[3];
 	b[1] = HEXTABLE[0x0f & c];	
 	b[0] = HEXTABLE[(c>>4) & 0x0f];
@@ -23,25 +31,27 @@ char * dec_to_hex(char c){
 	return b; 
 }
 
-int filter(char *buffer, int blen, char *str_filter, int len){
+int 
+filter(char *buffer, int blen, char *str_filter, int len){
 	if (str_filter[0] == 0)
 		return 1;
 #ifdef FILTER_ON
 	char *p = NULL;
 	buffer[blen] = 0;
-	if(blen < 52 || (p = (char*)memmem(buffer+52, blen-52, str_filter, 4)) == NULL ){
+	if(blen < 52 || 
+		(p = (char*) memmem(buffer+52, blen-52, str_filter, 4)) == NULL ){
 		return 0;
 	}
 	printf("Match[%s]\n",p);
 #endif
-		return 1;
+	return 1;
 }
 
-int getSrcIP(char *buffer, char *srcIP){
+int 
+getSrcIP(char *buffer, char *srcIP){
 	if(buffer == NULL){
 		return -1;
 	}
-
 	sprintf(srcIP,"%d.%d.%d.%d"
 		, buffer[12]
 		, buffer[13]
@@ -49,11 +59,12 @@ int getSrcIP(char *buffer, char *srcIP){
 		, buffer[15]);
 	return 0;
 }
-int getDstIP(char *buffer, char *dstIP ){
+
+int 
+getDstIP(char *buffer, char *dstIP ){
 	if(buffer == NULL){
 		return -1;
 	}
-
 	sprintf(dstIP,"%d.%d.%d.%d"
 		, buffer[16]
 		, buffer[17]
@@ -61,40 +72,47 @@ int getDstIP(char *buffer, char *dstIP ){
 		, buffer[19]);
 	return 0;
 }
-int getSrcPort(char *buffer){
+
+int 
+getSrcPort(char *buffer){
 	int port = 0;
 	port = 0x00ff & buffer[21];
 	port = port | (0xff00 & (buffer[20] << 8));
 	return src_port = port;
 }
 
-int getDstPort(char *buffer){
+int 
+getDstPort(char *buffer){
 	int port = 0;
 	port = 0x00ff & buffer[23];
 	port = port | (0xff00 & (buffer[22] << 8));
 	return dst_port = port;
 }
 
-int filter_port(int port ){
+int 
+filter_port(int port ){
 	if ( port < 0 ) 
 		return 1;
 #ifdef FILTER_ON
-	if( !((port == dst_port) || (port == src_port))){	
+	if(!((port == dst_port) || (port == src_port))){	
 		return 0;
 	}
 #endif	
 	return 1;
 }
-void dump(char *buffer, int len){
-	int i = 0, j = 0;
-	int Ishex = 1;	
-	for(i = 0; i < len; i++){
 
-		if( Ishex == 1 ){	
+void 
+dump(char *buffer, int len){
+	int i = 0, j = 0, Ishex = 1;
+	
+	for(i = 0; i < len; i++){
+		if( Ishex == 1 ){
+			
 			printf("%s ", dec_to_hex(buffer[i]));
 			if( ((i+1)%8 == 0) && i){
 				printf(" ");
 			}
+			
 			if(((i+1) % 16 == 0) && i || (i+1 == len)){	
 				j = (i+1) % 16;	
 				if(j){
@@ -109,6 +127,7 @@ void dump(char *buffer, int len){
 				else{ 
 					i = i - 16;	
 				}
+				
 				Ishex = 0;
 				j = 16 - j;	
 				while(j<16 && j--)
@@ -132,10 +151,12 @@ void dump(char *buffer, int len){
 	printf("\n");	
 }
 
-int getparam(int *fport, char *fdata,int *len, int argc, char *argv[]){
+int 
+getparam(int *fport, char *fdata, int *len, int argc, char *argv[]){
 	int i = 0;	
 	*fport = -1;
 	fdata[0] = 0;
+	
 	for(i =  1;i < argc; i++){
 		if( STREQ(argv[i],"-p")){
 			if( i+1 < argc){
@@ -162,22 +183,24 @@ int getparam(int *fport, char *fdata,int *len, int argc, char *argv[]){
 }
 
 
-int main(int argc, char *argv[] ) {
+int
+main(int argc, char *argv[] ) {
 	int i, recv_length, sockfd;
 	unsigned char buff[90000];
 	char dstIP[16];
 	char srcIP[16];
 	int fport;
 	int fdata_len= 0;
-	char fdata[256];	
+	char fdata[256];
+	
 	if(getparam (&fport, fdata, &fdata_len, argc, argv) != 0)	
 		return -1;
+		
 	if (( sockfd = socket(PF_INET, SOCK_RAW, IPPROTO_TCP)) == -1){
 		printf("ERROR: %s\n",strerror(errno));
 		return -1;}
 
-		
-	for (i=1; i ;){
+	for (;;){
 		//memset(buff,0,sizeof(buff));
 		recv_length =  recv(sockfd, buff, 80000, 0);
 		(void) getSrcPort(buff);
@@ -201,7 +224,7 @@ int main(int argc, char *argv[] ) {
 			dump(buff, recv_length);}
 		}
 
-	/*	
+	/* if you want to use pcap library 
 	struct pcap_pkthdr header;
 	pcap_t *pcap_handle;
 	const unsigned char *packet;
